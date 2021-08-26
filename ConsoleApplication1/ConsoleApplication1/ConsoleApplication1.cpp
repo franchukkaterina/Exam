3bb69c88f962b4405e2a9267c542db355a26c2b5
Franchuk <ketrinfren@gmail.com>
2021-08-26 20:36:59 +0300

    error ctime

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,1 +1,57 @@
-#include <iostrem>
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <iomanip> 
+#include <chrono> 
+#include <ctime> 
+#include <thread>
+#include  <time.h>
+
+using namespace sf;
+
+int main()
+{
+	srand(time(0));
+	RenderWindow window(VideoMode(400, 400), "Time");
+	
+	sf::Font font;
+	if (!font.loadFromFile("Summer Calling.ttf")) {
+		std::cout << "error";
+	}
+
+	time_t now = time(0);
+	size_t size = 34;
+	char* dt = ctime(&now);
+
+	sf::Text text;
+	text.setFont(font);
+	text.setString(dt);
+	text.setCharacterSize(24);
+	text.setFillColor(Color::White);
+
+
+	while (window.isOpen())
+	{
+		Event event;
+		window.clear(sf::Color::Black);
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed) {
+				window.close();
+			}
+			if (event.type == Event::KeyPressed) {
+				if (event.key.code == Keyboard::D) {
+					window.clear(sf::Color::Black);
+					text.setFillColor(Color::White);
+				}
+				if (event.key.code == Keyboard::S) {
+					window.clear(sf::Color::White);
+					text.setFillColor(Color::Black);
+				}
+			}
+
+		}
+		window.draw(text);
+		window.display();
+	}
+}
+
